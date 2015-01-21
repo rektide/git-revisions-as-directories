eb81065e914f9e7853736cf31548bde2e1f16ff3
rektide <rektide@voodoowarez.com>
2015-01-20 19:51:02 -0500

    Overlayfs implementation.

--- git-revisions-as-directories.sh
+++ git-revisions-as-directories.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+
 echo "output directory $1"
 echo $1
-mkdir -p $1
+mkdir -p "$1/.uppers"
 
 git branch
 
@@ -11,7 +12,8 @@ git branch
 for l in `git log --pretty=format:"%at-%H"`; do
     stamp=`echo $l | sed -n 's/\-\([^\-]*\)$//p'`
     sha=`echo $l | sed -n 's/^\([^\-]*\)\-//p'`
-    mkdir -p "$1/$stamp"
+    mkdir -p "$1/$stamp" "$1/.uppers/$stamp"
+    mount -t overlayfs -o lowerdir=$(pwd) upperdir="$1/.uppers/$stamp" overlayfs "$1/$stamp"
+    cd "$1/$stamp"
     git checkout $sha
-    cp -r * $1/$stamp
 done
